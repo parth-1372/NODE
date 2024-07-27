56df7782a1eacc412203b81b80051f093662a337
Parth <parthmungra1372@gmail.com>
2024-07-28 01:09:10 +0530

    bearer Token

--- Short-URL/middlewares/auth.js
+++ Short-URL/middlewares/auth.js
@@ -1,21 +1,31 @@
 
 const { getUser } = require("../service/auth")
 async function restrictedToLoggedinUserOnly(req,res,next){
-  const userUid = req.cookies?.uid;
-  if(!userUid) return res.redirect("/login");
-  const user = getUser(userUid);
+  const userUid = req.headers?.['Authorization'];
+  // const userUid = req.cookies?.uid;
+  if(!userUid) return res.redirect("/login")[1];
+  const token = userUid.spilt('Bearer ')
+  const user = getUser(token);
   if(!user) return res.redirect("/login");
 
   req.user=user;
   next();
 }
 
-async function checkAuth(req,res,next){
-   const userUid = req.cookies?.uid;
-  const user = getUser(userUid);
-  req.user=user;
-  next();
-}
+const checkAuth = (req, res, next) => {
+  const userUid = req.headers.authorization;
+  const token = userUid?.split('Bearer ')[1]; // Corrected line
+  
+  if (token) {
+    // Your token verification logic here
+    next();
+  } else {
+    res.status(401).json({ message: 'Authentication failed!' });
+  }
+};
+
+module.exports = checkAuth;
+
 
 module.exports = {
     restrictedToLoggedinUserOnly,

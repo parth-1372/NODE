1812373c61541666fc700ebabf4ded572c4ae6a9
Parth <parthmungra1372@gmail.com>
2024-07-28 00:27:31 +0530

    State Less Authtiecation Done

--- Short-URL/service/auth.js
+++ Short-URL/service/auth.js
@@ -1,11 +1,15 @@
-const sessionIdToUserMap = new Map();
-
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user);
+// const sessionIdToUserMap = new Map();
+const jwt  = require("jsonwebtoken");
+const secret="Parthm"
+function setUser(user){
+    return jwt.sign({
+        ...user
+    },secret);
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token){
+    if(!token) return null;
+    return jwt.verify(token,secret);
 }
 
 module.exports = {
